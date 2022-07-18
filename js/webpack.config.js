6b54a229244a4486bff26a85423b936d83f2790e
nafiulnipu <mnipu2@uic.edu>
2022-07-18 11:58:41 -0500

    dont work

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -22,26 +22,47 @@ module.exports = [
       module : {
         loaders : [
           {
-            test: /\.(js|jsx|ts|tsx)?$/,
+            test: /\.(js|jsx)?$/,
             exclude: /(node_modules|bower_components)/,
             loaders: [`babel?${JSON.stringify( babelSettings )}`]
+          },
+          {
+            test: /\.(ts|tsx)?$/,
+            loader: 'ts-loader',
+            exclude: /node_modules/,
           },
           // { test: /\.css$/, loader: "style-loader?sourceMap!css-loader?importLoaders=1" },
           {
             test: /\.json$/, loader: 'json-loader'
           },
           {
-            test: /\.s?[ac]ss$/,
-            loaders: ['style-loader', 'css-loader', 'sass-loader'],
-            exclude: [/node_modules/],
-          },
-            {
-              test: /\.css$/,
-              loaders: [
-                'style-loader',
-                'css-loader'
-              ]
-            }
+            test: /\.(scss)$/,
+            use: [{
+              // inject CSS to page
+              loader: 'style-loader'
+            }, {
+              // translates CSS into CommonJS modules
+              loader: 'css-loader'
+            }, {
+              // Run postcss actions
+              loader: 'postcss-loader',
+              options: {
+                // `postcssOptions` is needed for postcss 8.x;
+                // if you use postcss 7.x skip the key
+                postcssOptions: {
+                  // postcss plugins, can be exported to postcss.config.js
+                  plugins: function () {
+                    return [
+                      require('autoprefixer')
+                    ];
+                  }
+                }
+              }
+            }, {
+              // compiles Sass to CSS
+              loader: 'sass-loader'
+            }]
+          }
 
         ]
       }
